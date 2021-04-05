3176128060db81f7d6d9cba14978b35e1a5a3f9c
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-04-05 12:06:42 +0300

    переписує contacts-actions на Redux Toolkit

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,24 +1,47 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+// import contactsActionTypes from './contacts-action-types';
+
+// const addContact = (name, number) => ({
+//   type: contactsActionTypes.ADD,
+//   payload: {
+//     name,
+//     number,
+//     id: uuidv4(),
+//   },
+// });
+
+// const deleteContact = id => ({
+//   type: contactsActionTypes.DELETE,
+//   payload: id,
+// });
+
+// const filterContactsByName = value => ({
+//   type: contactsActionTypes.FILTER,
+//   payload: value,
+// });
+
+// const contactsActions = {
+//   addContact,
+//   deleteContact,
+//   filterContactsByName,
+// };
+
+// export default contactsActions;
+
+// ____________________________________________________________________________________________________________
+
+import { createAction, nanoid } from '@reduxjs/toolkit';
 import contactsActionTypes from './contacts-action-types';
 
-const addContact = (name, number) => ({
-  type: contactsActionTypes.ADD,
+const addContact = createAction(contactsActionTypes.ADD, (name, number) => ({
   payload: {
     name,
     number,
-    id: uuidv4(),
+    id: nanoid(),
   },
-});
-
-const deleteContact = id => ({
-  type: contactsActionTypes.DELETE,
-  payload: id,
-});
-
-const filterContactsByName = value => ({
-  type: contactsActionTypes.FILTER,
-  payload: value,
-});
+}));
+const deleteContact = createAction(contactsActionTypes.DELETE);
+const filterContactsByName = createAction(contactsActionTypes.FILTER);
 
 const contactsActions = {
   addContact,
