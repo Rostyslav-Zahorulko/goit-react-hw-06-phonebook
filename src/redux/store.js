390542c56c9c89cc8295f943324d642802f7355a
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2021-04-05 11:35:14 +0300

    переписує store на Redux Toolkik

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,32 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import contactsReducer from './contacts/contacts-reducer';
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
+
+// const store = createStore(rootReducer, composeWithDevTools());
+
+// export default store;
+
+// ____________________________________________________________________________________________________________
+
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import contactsReducer from './contacts/contacts-reducer';
 
-const rootReducer = combineReducers({
+const reducer = {
   contacts: contactsReducer,
-});
+};
 
-const store = createStore(rootReducer, composeWithDevTools());
+const middleware = getDefaultMiddleware =>
+  getDefaultMiddleware().concat(logger);
+
+const store = configureStore({
+  reducer,
+  devTools: process.env.NODE_ENV === 'development',
+  middleware,
+});
 
 export default store;
